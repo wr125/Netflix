6448d22aa4d9d993b36e96e10443af91a8e8ab2c
Robert Willis <robert.willis010@gmail.com>
2023-06-02 20:51:04 +0100

    first commit

---
+++ src/pages/browse.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowseContainer } from '../containers/browse';
+import { useContent } from '../hooks';
+import { selectionFilter } from '../utils';
+
+ function Browse() {
+  const { series } = useContent('series');
+  const { films } = useContent('films');
+  const slides = selectionFilter({ series, films });
+
+  return <BrowseContainer slides={slides} />;
+}
+
+export default Browse;
